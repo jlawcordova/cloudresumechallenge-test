29a12f798d3cc10d98c605d4560f42893b58710c
J. Law. Cordova <jlaw.cordova@gmail.com>
2023-03-09 00:10:12 +0800

    test: increase view count

--- cypress/e2e/web/resume.cy.ts
+++ cypress/e2e/web/resume.cy.ts
@@ -6,10 +6,21 @@ describe("Resume", () => {
   });
 
   it("should contain the name of the resume owner", () => {
-    cy.get("h1").contains("Junel Lawrence Cordova");
+    cy.get("h1").should("contain", "Junel Lawrence Cordova");
   });
 
-  it("should show the view count", () => {
-    cy.get(".view-count").contains("views");
+  it("should increase the view count when visited", () => {
+    var viewCount: number;
+
+    cy.get(".view-count").then((content) => {
+      viewCount = parseInt(content[0].innerText.split(" ")[0]);
+    });
+
+    // Revisit the same page to increase the view count.
+    cy.visit("https://resume.jlawcordova.com");
+    cy.get(".view-count").then((content) => {
+      var newViewCount = parseInt(content[0].innerText.split(" ")[0]);
+      cy.wrap(newViewCount).should("be.greaterThan", viewCount);
+    });
   });
 });
